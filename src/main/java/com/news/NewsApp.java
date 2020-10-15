2f35dd78bd665671b74736404b19568c7e04a85e
r.popov <rom2517pop@gmail.com>
2020-10-15 15:02:25 +0300

    Created writer screen.

--- src/main/java/com/news/NewsApp.java
+++ src/main/java/com/news/NewsApp.java
@@ -4,7 +4,6 @@ import com.news.view.ScreenWindow;
 
 public class NewsApp {
     public static void main(String[] args) {
-        ScreenWindow window = new ScreenWindow("Newsletter");
-        window.setVisible(true);
+        new ScreenWindow();
     }
 }
--- src/main/java/com/news/view/ScreenWindow.java
+++ src/main/java/com/news/view/ScreenWindow.java
@@ -12,37 +12,38 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ScreenWindow extends Frame implements ActionListener, WindowListener {
 
-    private DatagramSocket socket;
-    private DatagramPacket packet;
+    private final String NEWSLETTER = "Отправка новостей";
+    private final String ARCHIVE = "Архив новостей";
+    private final String NEW_LABEL = "Новая новость";
+    private final String SEND_LABEL = "Опубликовать";
+    private Button send;
     private TextArea archiveForm;
     private TextField inputText;
-    private Label archiveLabel;
-    private Label newNewsLabel;
-    private Button send;
-    private Button exit;
+    private DatagramSocket socket;
     private ServerThread serverThread;
 
-    public ScreenWindow(String header) throws HeadlessException {
-        super(header);
-        setSize(500, 500);
+    public ScreenWindow() throws HeadlessException {
+        setSize(500, 400);
         archiveForm = new TextArea(10, 30);
         inputText = new TextField(30);
-        send = new Button("Опубликовать");
-        exit = new Button("Exit");
+        send = new Button(SEND_LABEL);
         send.addActionListener(this);
-        this.addWindowListener(this);
-        send.setEnabled(false);
+        addWindowListener(this);
         GridBagLayout gbl = new GridBagLayout();
         GridBagConstraints gbc = new GridBagConstraints();
         setLayout(gbl);
+        setTitle(NEWSLETTER);
+        createArchiveLabel(gbl, gbc, new Label(ARCHIVE));
         addArchiveArea(gbl, gbc, archiveForm);
-        addExitButton(gbl, gbc, exit);
+        createNewNewsLabel(gbl, gbc, new Label(NEW_LABEL));
         addTextField(gbl, gbc, inputText);
         addSendButton(gbl, gbc, send);
-
+        setVisible(true);
         serverThread = new ServerThread(this);
         serverThread.start();
         try {
@@ -52,43 +53,58 @@ public class ScreenWindow extends Frame implements ActionListener, WindowListene
         }
     }
 
-    private void addArchiveArea(GridBagLayout gbl, GridBagConstraints gbc, TextArea ta) {
-        gbc.anchor = GridBagConstraints.NORTHWEST;
-        gbc.fill = GridBagConstraints.BOTH;
-        gbc.gridheight = 1;
-        gbc.gridwidth = 1;
+    private void createArchiveLabel(GridBagLayout gbl, GridBagConstraints gbc, Label label) {
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.NONE;
         gbc.gridx = GridBagConstraints.RELATIVE;
         gbc.gridy = GridBagConstraints.RELATIVE;
-        gbc.insets = new Insets(10, 10, 10, 10);
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbl.setConstraints(label, gbc);
+        add(label);
+    }
+
+    private void createNewNewsLabel(GridBagLayout gbl, GridBagConstraints gbc, Label label) {
+        gbc.anchor = GridBagConstraints.SOUTH;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = GridBagConstraints.RELATIVE;
+        gbc.gridy = 2;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbl.setConstraints(label, gbc);
+        add(label);
+    }
+
+    private void addArchiveArea(GridBagLayout gbl, GridBagConstraints gbc, TextArea ta) {
+        gbc.anchor = GridBagConstraints.NORTH;
+        gbc.fill = GridBagConstraints.BOTH;
+        gbc.gridx = GridBagConstraints.RELATIVE;
+        gbc.gridy = 1;
+        gbc.insets = new Insets(0, 10, 15, 10);
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbc.weightx = 1.0;
         gbc.weighty = 1.0;
+        ta.setEditable(false);
         gbl.setConstraints(ta, gbc);
         add(ta);
     }
 
-    private void addExitButton(GridBagLayout gbl, GridBagConstraints gbc, Button exit) {
-        gbc.weightx = 0;
-        gbc.weighty = 0;
-        gbc.fill = GridBagConstraints.NONE;
-        gbc.gridwidth = GridBagConstraints.REMAINDER;
-        gbc.gridheight = 1;
-        gbc.ipadx = 10;
-        gbc.gridx = GridBagConstraints.RELATIVE;
-        gbc.gridy = GridBagConstraints.RELATIVE;
-        gbl.setConstraints(exit, gbc);
-        add(exit);
-    }
-
     private void addTextField(GridBagLayout gbl, GridBagConstraints gbc, TextField it) {
+        gbc.anchor = GridBagConstraints.NORTH;
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.gridx = GridBagConstraints.RELATIVE;
-        gbc.gridwidth = 1;
+        gbc.gridy = 3;
+        gbc.gridwidth = GridBagConstraints.REMAINDER;
+        gbc.weightx = 1.0;
+        gbc.weighty = 1.0;
+        gbc.insets = new Insets(0, 10, 10, 10);
         gbl.setConstraints(it, gbc);
         add(it);
     }
 
     private void addSendButton(GridBagLayout gbl, GridBagConstraints gbc, Button send) {
+        gbc.anchor = GridBagConstraints.EAST;
         gbc.fill = GridBagConstraints.NONE;
+        gbc.gridx = GridBagConstraints.RELATIVE;
+        gbc.gridy = 4;
         gbc.gridwidth = GridBagConstraints.REMAINDER;
         gbl.setConstraints(send, gbc);
         add(send);
@@ -99,7 +115,8 @@ public class ScreenWindow extends Frame implements ActionListener, WindowListene
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(send)) {
             String message = inputText.getText();
-            archiveForm.append("\n:" + message);
+            SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+            archiveForm.append("\n" + format.format(new Date()) + " -> " + message);
             sendString(message);
         }
     }
@@ -107,7 +124,8 @@ public class ScreenWindow extends Frame implements ActionListener, WindowListene
     private void sendString(String message) {
         byte[] buff = message.getBytes();
         try {
-            packet = new DatagramPacket(buff, buff.length, InetAddress.getByName("255.255.255.255"), 8800);
+            DatagramPacket packet = new DatagramPacket(buff, buff.length,
+                    InetAddress.getByName("255.255.255.255"), 8800);
             socket.send(packet);
         } catch (IOException e) {
             e.printStackTrace();
@@ -117,7 +135,8 @@ public class ScreenWindow extends Frame implements ActionListener, WindowListene
     @Override
     public void windowClosing(WindowEvent e) {
         setVisible(false);
-        System.exit(1);
+        socket.close();
+        System.exit(0);
     }
 
     @Override
